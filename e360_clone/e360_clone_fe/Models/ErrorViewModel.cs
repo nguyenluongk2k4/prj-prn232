8b181cc665bcbc29de32ac58f1d0663a5ff0a4f4
luongnguyen <nguyenluongk2k4@gmail.com>
2026-03-15 09:04:20 +0700

    init new prj

---
+++ e360_clone/e360_clone_fe/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace e360_clone_fe.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
